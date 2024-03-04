a17f07c7e9e17d2ae6f2bb8d7ca4b7f17824e3a7
ChristianVazquezGarcia <cnjvgarcia@gmail.com>
2024-03-04 01:15:05 -0300

    Entrega OFICIAL sprint3

--- assets/script/fav.js
+++ assets/script/fav.js
@@ -1,7 +1,7 @@
 import {card, renderCards} from "./module/functions.js"
 const main = document.getElementById("mainFav")
 
-//let divContenedorMovie = document.getElementById("contenedorMovie")
+
 
 const url = "https://moviestack.onrender.com/api/movies"
 const init = {
@@ -16,32 +16,26 @@ fetch ( url, init )
 .then( datos =>{
     //si desclickea que pase esto
     let moviesFavs = JSON.parse(localStorage.getItem("favoritas")) || [] //Tengo el array de favoritas
-//Traigo del local storage las pelis enteras con el fav y le cambio a la que elimine de favs, el fav true por false :)
+
     let todasLasMovies =JSON.parse(localStorage.getItem("peliculasNoFaveadas")) || []
     //creo array nuevo donde van a ir todas las pelis despues de desfavear
     let todasLasMoviesFavyNofav=[]
 if (todasLasMovies.length!=0) {
     todasLasMoviesFavyNofav=todasLasMovies
 }
-    console.log(moviesFavs);
+    
 
     let cardsFavs = card(moviesFavs)
-            renderCards(cardsFavs,main )
+    renderCards(cardsFavs,main )
 
     main.addEventListener("input", (e)=>{
-        
-        console.log(e.target.checked);
         if (e.target.checked==false) {// si desclickeo
             let b = e.target.dataset.id; //que me de el id de la peli que desclickeo
-
             //me borra de favoritos la pelicula
             let movieFav = moviesFavs.filter(movie=>movie.id!=b)//Que me devuelva un array sin esa pelicula
             moviesFavs=movieFav
-            console.log(moviesFavs);
             cardsFavs = card(moviesFavs)
-            console.log(cardsFavs);
-            renderCards(cardsFavs,main )
-            
+            renderCards(cardsFavs,main)
             todasLasMoviesFavyNofav=todasLasMovies.map(movie=>{
                 if (movie.id==b) {
                     return { ...movie, fav: false }
@@ -53,21 +47,9 @@ if (todasLasMovies.length!=0) {
             })
             localStorage.setItem("peliculasNoFaveadas",JSON.stringify(todasLasMoviesFavyNofav))
             todasLasMovies=todasLasMoviesFavyNofav
-            localStorage.setItem("favoritas",JSON.stringify(moviesFavs))
-
-            //quiero que me recorra el array que tengo en el local storage y me filtre por los que desclickeo
-            //osea donde tengo datos.movies pongo el array que tengo en el local storage
-        
-            
+            localStorage.setItem("favoritas",JSON.stringify(moviesFavs))    
         }
-        
-        
     })
-    //si no clickeo, que me imprima la lista del local storage
-
-    
-            
-
 })
 .catch(err=>console.log(err))
 
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,139 +1,26 @@
 import {card, renderCards, createOptionsSelect, filtrarPorNombre, filterForGenre} from "./module/functions.js"
-//Crea las tarjetas funcion card
+
 
 
 
 //CreoElContenedor dodne voy a almacenar los cards
 let divContenedorMovie = document.createElement("div");
 divContenedorMovie.setAttribute("id","contenedorMovie");
-
 let mainFav = document.getElementById("mainFav")
 console.log(mainFav);
-
-//introduce las tarjetas en el html
-
-
-//Agrego al div contenedor las cards
-
-
-
-
-//Agrego al main el div contenedor
-//1)primero capturo el main
-
 let main =document.querySelector("main");
-
-//2)le asigno a main que le voy a agregar
 
 main.append(divContenedorMovie);
 /*********************************************************** */
 
-//Modifico las clases del contenedor
-//Preguntar al profe porque me salta error cuando pongo classList.add
+
 divContenedorMovie.className=" flex flex-wrap w-4/5 gap-8 justify-center rounded-md max-md:flex-col md:shadow-lg md:shadow-indigo-900 ";
 
-//Modifico las clases de los articulos
-//1)obtengo el article
 let article= divContenedorMovie.querySelectorAll("article")
-//esto me devolvio un array, recorro ese array de articles y les asigno clases
 
-/**
- * Profe, mejore el codigo en base a lo que me dijo 
- * la otra vez y ahora agregue las clases directo en el
- * texto en vez de agregarla con funciones. Dejo las funciones comentadas
-
-function asignoClases(vec, clases){
-    //recorro el array de articulos
-    let aux=" "
-    for (const iterator of vec) {
-        aux=iterator
-        aux+=iterator.classList.add(clases)
-        
-    }
-}
-
-
-function sobreEscriboClases(vec, clases) {
-    let aux=" "
-    for (const iterator of vec) {
-        aux=iterator
-        aux+=iterator.className=clases;
-        
-    }
-}
- */
-
-// let h4 =divContenedorMovie.querySelectorAll("article h4");
-
-// let h6 =divContenedorMovie.querySelectorAll("article>h6");
-
-// let pArticulos=divContenedorMovie.querySelectorAll("article>p");
-
-
-
-//le pongo clases al main
 main.className="flex flex-col items-center gap-16"
 
  
-
-//Como lo hago?
-/**
- * 1)entro al vector de objetos y a su vez entro a la propiedad de generos
- * 2)si el genero encontrado es igual con el del valor, entonces me quedo con el objeto.
- * 3)Donde pongo el objeto?
- * en un nuevo vector
- * 4)Recorro el vector nuevo preguntando si el objeto seleccionado es del mismo nombre buscado
- * 
- * 5)si lo es, utilizo la funcion Cards y le introduzco el nuevo vector con la pelicula ya "Filtrada"
- * o puedo hacer una nueva funcion exactmente igual para un solo objetp
- * 6) si no lo es entonces pongo q no existe pelicula con ese nombre
- * ----------------
- * 
- */
-
-
-
-
-
-//Busco todos los tipos de generos y los insertos en el select
-/** 2*/
-
-
-
- 
-
-
-
-
-
-
-
-
-//busco el valor seleccionado en el select
-//con el valor seleccionado busco la/s pelicula/s q coincidan
-//muestro las peliculas
-
-
-
-
-
-
-
-
-
-/**
- * const filtroNombre = filtrarPorNombre(moviesList, search.value)
-    const filterMovieForGenre = filterForGenre(filtroNombre, select.value)
-    let movie = card(filterMovieForGenre)
-    renderCards(movie, divContenedorMovie)
- * 
- * 
- */
-
-
-    //Agrego el detalle en las cards de las pelis
-
-
 
 const url = "https://moviestack.onrender.com/api/movies"
 const init = {
@@ -167,13 +54,10 @@ fetch( url, init )
             peliculasPaMostrar=peliculas10
         }
         
-        console.log(arrayMoviesFav);
         
-       console.log(peliculasPaMostrar);
         
         divContenedorMovie.addEventListener("input", (e)=>{
             let a = e.target.checked
-            console.log(a);
             if (a) {
                 console.log(a)
                     peliculasCompletas = peliculas10.map(movie=>{
@@ -193,8 +77,8 @@ fetch( url, init )
                     
         
                     let b = e.target.dataset.id;
-                        let movieFav= peliculas10.find(movie=>movie.id==b)
-                        arrayMoviesFav.push(movieFav)
+                    let movieFav= peliculas10.find(movie=>movie.id==b)
+                    arrayMoviesFav.push(movieFav)
 
                     localStorage.setItem("favoritas",JSON.stringify(arrayMoviesFav))     
             }
@@ -213,31 +97,23 @@ fetch( url, init )
                 peliculas10=peliculasCompletas
                 renderCards(card(peliculas10), divContenedorMovie)
                 let b = e.target.dataset.id;
-                        let movieFav= arrayMoviesFav.filter(movie=>movie.id!=b)
-                        arrayMoviesFav=movieFav
-                    localStorage.setItem("favoritas",JSON.stringify(arrayMoviesFav)) 
+                let movieFav= arrayMoviesFav.filter(movie=>movie.id!=b)
+                arrayMoviesFav=movieFav
+                localStorage.setItem("favoritas",JSON.stringify(arrayMoviesFav)) 
             }
             
         })
 
-
-
-
-
-
         //Aca van peliculaspamostrar
         let movies = card(peliculasCompletas)
         renderCards(movies, divContenedorMovie)
-        //aca tambien va peliculasPaMostrar
-        let genres= new Set (peliculasCompletas.map(movie=>movie.genres).flat())/*1*/
+        let genres= new Set (peliculasCompletas.map(movie=>movie.genres).flat())
         let select = document.getElementById("selectGenre")
         createOptionsSelect(genres,select)
 
         let search = document.getElementById("search")
 
-        search.addEventListener("input", ()=> {/**3*/
-        //aca tambien va peliculasPaMostrar
-
+        search.addEventListener("input", ()=> {
         //Ejecuto la funcion de filtrarPorNombre
         const filtroNombre = filtrarPorNombre(peliculasCompletas, search.value)
         let movie
@@ -253,9 +129,7 @@ fetch( url, init )
         } )
 
 
-        select.addEventListener("input", ()=>{/*** 4*/
-        //aca tambien va peliculasPaMostrar
-
+        select.addEventListener("input", ()=>{
         const filtroNombre = filtrarPorNombre(peliculasCompletas, search.value)
         let movie
         if(select.value=="genre"){
@@ -268,11 +142,6 @@ fetch( url, init )
         renderCards(movie, divContenedorMovie)
         }
         })
-
-
-       
-        
-
        
     } )
     .catch( err => console.log(err) ) 
